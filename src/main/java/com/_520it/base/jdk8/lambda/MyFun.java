722bc8cdf03f16a95f2a8489ef68b0f26a1ba826
superman <superman@163.com>
2020-06-01 17:16:43 +0800

    删除

--- src/main/java/com/_520it/base/jdk8/lambda/MyFun.java
+++
@@ -1,8 +0,0 @@
-package com._520it.base.jdk8.lambda;
-
-@FunctionalInterface
-public interface MyFun {
-
-	public Integer getValue(Integer num);
-	
-}
--- src/main/java/com/_520it/base/jdk8/lambda/MyPredicate.java
+++
@@ -1,8 +0,0 @@
-package com._520it.base.jdk8.lambda;
-
-@FunctionalInterface
-public interface MyPredicate<T> {
-
-	public boolean test(T t);
-	
-}
